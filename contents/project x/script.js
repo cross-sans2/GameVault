aab31b3ddf289eb1f4ade425e58e597f48d6ea83
cross-sans2 <cts.neolang.br@gmail.com>
2023-05-29 14:27:05 -0300

    updates

---
+++ contents/project x/script.js
@@ -0,0 +1,48 @@
+loading_screen = document.getElementById('loading-screen')
+function goto(nodeId) {
+    const nodes = document.getElementsByTagName('loc')
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].style.display = "none"; // Hide all nodes
+    }
+    const targetNode = document.getElementById(nodeId);
+    if (targetNode) {
+        targetNode.style.display = "block"; // Show the target node
+        window["nodepos"] = nodeId
+    } else {
+        alert(`node "${nodeId}" not found, reseting to last known position`)
+        goto(window["nodepos"])
+    }
+}
+
+function password(code, act) {
+    const enteredPassword = prompt("Enter password:", "");
+    if (enteredPassword === code) {
+        new Function(act)()
+    } else {
+        alert("Wrong password. Access denied.");
+    }
+}
+
+function link(node)
+{
+    node.innerHTML = node.innerHTML.replace(
+        /\[\[(.*)\|(.*)\]\]/
+        ,
+        `<button onclick="goto('$2')">$1</button>`
+    )
+    node.innerHTML = node.innerHTML.replace(
+        /\[\:(.*)\|(.*),(.*)\:\]/
+        ,
+        `<button onclick=password('$2','$3')>$1</button>`
+    ).replaceAll("\n",'<br>')
+}
+locations = document.getElementsByTagName('loc')
+function load(){
+    for(l of locations){
+        l.style.display = 'none'
+        link(l)
+    }
+    goto("main")
+    loading_screen.style.display = 'none'
+}
+load()
